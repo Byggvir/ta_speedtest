8a8281f07c0f9ce5c577c11fb15072091ea217b4
Byggvir <thomas@arend-rhb.de>
2018-07-29 17:48:06 +0200

    fetch-row changed to fetch_assoc.

--- html/speedreport.php
+++ html/speedreport.php
@@ -1,36 +1,13 @@
 <!DOCTYPE html>
 <html lang="de-DE" class="no-js">
 <head>
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Speedtest</title>
-<style type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+    <title>Speedtest</title>
 
-table {
-  
-  border : 2px solid blue;
-  
-}
-td {
-  padding : 0.2em;
-  margin: 0.3em
-  
-}
-.verybig {
-   font-size :5em;
-
-}
-
-.parameter {
-  background : lightgray ;
-
-}
-
-.result {
-  text-align : right ;
-}
-
-</style>
 </head>
 <body>
 <h1>Speedtest-Reports</h1>
@@ -49,13 +26,13 @@ $querylast= "SELECT * FROM reports ORDER BY start DESC LIMIT 1;";
 /* Select queries return a resultset */
 if ($result = $mysqli->query($querylast)) {
  
-  while ($row = $result->fetch_row()) {
+  while ($row = $result->fetch_assoc()) {
 
 ?>
-<tr><td class="parameter">Ping:</td><td class="result"><?php echo sprintf("%0.3f",$row[6]); ?></td><td>ms</td></tr>
-<tr><td class="parameter">Download:</td><td class="result"><?php echo sprintf("%0.3f", $row[7]/1000000) ; ?></td><td>MBit/s</td></tr>
-<tr><td class="parameter">Upload:</td><td class="result"><?php echo sprintf("%0.3f",$row[8]/1000000) ; ?></td><td>MBit/s</td></tr>
-<tr><td class="parameter">Datum:</td><td class="result" colspan="2"><?php echo preg_replace("/ /","<br />",$row[4]); ?></td></tr>
+<tr><td class="parameter">Ping:</td><td class="result"><?php echo sprintf("%0.3f",$row["ping"]); ?></td><td>ms</td></tr>
+<tr><td class="parameter">Download:</td><td class="result"><?php echo sprintf("%0.3f", $row["download"]/1000000) ; ?></td><td>MBit/s</td></tr>
+<tr><td class="parameter">Upload:</td><td class="result"><?php echo sprintf("%0.3f",$row["upload"]/1000000) ; ?></td><td>MBit/s</td></tr>
+<tr><td class="parameter">Datum:</td><td class="result" colspan="2"><?php echo preg_replace("/ /","<br />",$row["start"]); ?></td></tr>
 <?php
 
   }
